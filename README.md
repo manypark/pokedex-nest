ac28f5e99bfafb5c75c581f8b18ca5082dc229b7
Manu <manypark@live.com>
2022-08-29 23:21:57 -0500

    validacion de variables de entorno con joi

--- README.md
+++ README.md
@@ -14,7 +14,17 @@ npm i
 ```
 docker-compose up -d
 ```
-5. Recostruir la db con la semilla
+
+5. Clonar el archivo ```.env.template``` y renombrar la copia a ```.env````
+
+6. Llenar las variables de entorno definidas en le ```.env```
+
+7. Ejecutar la aplicacion en dev:
+```
+npm run start:dev
+```
+
+8. Reconstruir la db con la semilla
 ```
 http://localhost:3000/api/v2/seed (GET)
 ```
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,13 @@ import { SeedModule } from './seed/seed.module';
 import { EnvConfiguration } from './config/env.config';
 import { CommonModule } from './common/common.module';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [ EnvConfiguration ],
+      load            : [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema
     }),
     MongooseModule.forRoot(env.MONGO_DB),
     PokemonModule,
